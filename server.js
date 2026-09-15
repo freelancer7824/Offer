488c3f1171555048251c3394c22673f15d5a9acd
SHOPON HASAN PRIYO <freelancer7824@gmail.com>
2026-09-16 02:37:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;  // ✅ ঠিক করা
 const DB_FILE = path.join(__dirname, 'users.json');
 
 app.use(express.json());
@@ -72,5 +72,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server running: http://localhost:' + PORT);
+    console.log('Server running on port ' + PORT);
 });
